94c5a6143752430cdf86a7f24efc783219178a7a
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2019-02-25 22:38:15 -0500

    Updated q1

--- q1.py
+++ q1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 # q1 Start
-q2 = pd.read_csv('yelp_academic_dataset_business.tsv', sep='\t').city.dropna()
-cnt = s[s.str.contains('las vegas', case=False)].count()
+business = pd.read_csv('../yelp_academic_dataset_business.tsv', sep='\t').city.dropna()
+cnt = business[business.str.contains('las vegas', case=False)].count()
 print(cnt)
