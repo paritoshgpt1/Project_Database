a7433cd25173c360681cd102fed0b91390d83312
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2019-02-26 00:15:14 -0500

    fix q4.py

--- q4.py
+++ q4.py
@@ -6,4 +6,5 @@ import sys
 conn = MySQLdb.connect("localhost", "clouduser", "dbroot", "yelp_db")
 query = "SELECT review_count, stars FROM businesses"  # the SQL query
 df = pd.read_sql(query, con=conn)
-df.describe().to_csv(sys.stdout, encoding='utf-8', float_format='%.2f')
+df.describe().to_csv(sys.stdout, encoding='utf-8',
+                     float_format='%.2f', index=False)
