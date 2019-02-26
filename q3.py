5dbf44cb11a82bc20a79dcc6f9c09e5dcfe9f271
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2019-02-25 22:58:48 -0500

    Fix q3

--- q3.py
+++ q3.py
@@ -1,14 +1,9 @@
 import pandas as pd
-import numpy as np
 
 # q3 Start
 tips = pd.read_csv('../yelp_academic_dataset_tip.tsv', sep='\t')
-groupedTips = tips.groupby(['user_id'])
-filteredTips = groupedTips.filter(lambda s: s['likes'].sum() > 0)
-tippedUsers = filteredTips['user_id'].unique()
-
 reviews = pd.read_csv('../yelp_academic_dataset_review.tsv', sep='\t')
-reviewUsers = reviews['user_id'].unique()
 
-commonUsers = np.intersect1d(tippedUsers, reviewUsers)
-print(len(commonUsers))
+merged = pd.merge(tips, reviews, how='inner', on='user_id')
+grouped = merged.groupby(['user_id'])
+print(len(grouped))
